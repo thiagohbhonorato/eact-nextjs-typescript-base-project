f2ebb8e2dbcb8a13411d2bdc0289520183a192f5
Thiago Honorato <thiagohbhonorato@gmail.com>
2021-04-14 12:00:23 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+
+import Logo from '../assets/logo.svg'
+import { Container } from '../../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>ReactJS Base Project</title>
+      </Head>
+
+      <Logo />
+      <h1>ReactJS Base Project</h1>
+      <p>A ReactJS + Next.js base project made by Thiago Honorato.</p>
+    </Container>
+  )
+}
+
+export default Home
