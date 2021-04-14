29b4047ce2e075d08f07c90d195e5bd1117acfd9
Thiago Honorato <thiagohbhonorato@gmail.com>
2021-04-14 12:05:53 -0300

    reorder directories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 
 import Logo from '../assets/logo.svg'
-import { Container } from '../../styles/pages/Home'
+import { Container } from '../styles/pages/Home'
 
 const Home: React.FC = () => {
   return (
